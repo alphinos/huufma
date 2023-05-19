f8f1ceba37a53e06f23899f1bc5b550aef457e6d
Carlos Veras <pessoal.carlosveras@gmail.com>
2023-05-19 13:40:02 -0300

    Atualizando o solver

--- solver.py
+++ solver.py
@@ -38,13 +38,28 @@ def solverHU(file_path: str) -> None:
     x = m.addVars(qtd_Patients, 3, qtdBeds, vtype = GRB.BINARY)
 
     #Objective function
+    sum_prioridade = 0
+    for i1 in I:
+        for i2 in I:
+            if i1 == i2:
+                continue
+            for j in J:
+                for k in K_Types[j]:
+                    # Cada item de patients é uma lista
+                    # Na posição 0, está a chance de sobrevivência
+                    # Na posição 1, está uma lista com as prioridades de cada paciente
+                    # para cada tipo de leito
+                    if patients[i1][1][j] > patients[i2][1][j]:
+                        sum_prioridade += x[i1, j, k] - x[i2, j, k]
+                        
+
     sum_ = 0
     for i in I:
         for j in J:
             for k in K_Types[j]:
                 sum_ += x[i, j, k] * patients[i][0]
 
-    m.setObjective( sum_, sense = grbp.GRB.MAXIMIZE )
+    m.setObjective( sum_ + sum_prioridade, sense = grbp.GRB.MAXIMIZE )
 
     #Constraints
 
@@ -75,20 +90,20 @@ def solverHU(file_path: str) -> None:
         m.addConstr( sum_c3 <= q[j] )
     
     # C4
-    for i1 in I:
-        for i2 in I:
-            if i1 == i2:
-                continue
-            for j in J:
-                for k in K_Types[j]:
-                    # Cada item de patients é uma lista
-                    # Na posição 0, está a chance de sobrevivência
-                    # Na posição 1, está uma lista com as prioridades de cada paciente
-                    # para cada tipo de leito
-                    if patients[i1][1][j] > patients[i2][1][j]:
-                        m.addConstr(
-                            x[i1, j, k] >= x[i2, j, k]
-                        )
+    # for i1 in I:
+    #     for i2 in I:
+    #         if i1 == i2:
+    #             continue
+    #         for j in J:
+    #             for k in K_Types[j]:
+    #                 # Cada item de patients é uma lista
+    #                 # Na posição 0, está a chance de sobrevivência
+    #                 # Na posição 1, está uma lista com as prioridades de cada paciente
+    #                 # para cada tipo de leito
+    #                 if patients[i1][1][j] > patients[i2][1][j]:
+    #                     m.addConstr(
+    #                         x[i1, j, k] >= x[i2, j, k]
+    #                     )
     
     #Execute
     m.optimize()
